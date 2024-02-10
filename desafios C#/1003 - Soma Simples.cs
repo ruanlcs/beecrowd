ac3ad88e2098ae489c64bd3e1cfaf35bc26e6a2b
Ruan Lucas <142461245+ruanlcs@users.noreply.github.com>
2024-02-09 21:32:00 -0300

    Create 1003 - Soma Simples.cs

---
+++ desafios C#/1003 - Soma Simples.cs
@@ -0,0 +1,15 @@
+//Author: @ruanlcs
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int A = Int32.Parse(Console.ReadLine());
+        int B = Int32.Parse(Console.ReadLine());
+        int sum = A + B;
+        Console.WriteLine("SOMA = " + sum);
+
+    }
+
+}
