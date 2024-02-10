9fef6f94636b7004e1dedb9dda73e4614a853804
Ruan Lucas <142461245+ruanlcs@users.noreply.github.com>
2024-02-09 21:33:07 -0300

    Create  1004 - Produto Simples.cs

---
+++ desafios C#/1004 - Produto Simples.cs
@@ -0,0 +1,15 @@
+//Author: @ruanlcs
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        int PROD = number1 * number2;
+        Console.WriteLine("PROD = " + PROD);
+
+    }
+
+}
