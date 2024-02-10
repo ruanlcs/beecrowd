bd649c5fd71387afd319a231c706240be0dbb81d
Ruan Lucas <142461245+ruanlcs@users.noreply.github.com>
2024-02-09 21:25:26 -0300

    Create 1001 - Extremamente Básico.cs

---
+++ desafios C#/1001 - Extremamente Básico.cs
@@ -0,0 +1,15 @@
+//Author: @ruanlcs
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+         int A = Int32.Parse(Console.ReadLine());
+         int B = Int32.Parse(Console.ReadLine());
+         int X = A + B;
+         Console.WriteLine("X = " + X);
+
+    }
+
+}
