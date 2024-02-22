b23b7afd5af2155b8de0a50290de2a3bb32cc0a6
Ruan Lucas <142461245+ruanlcs@users.noreply.github.com>
2024-02-21 21:01:55 -0300

    Create 1016 - Distância.cs

---
+++ desafios C#/1016 - Distância.cs
@@ -0,0 +1,16 @@
+//Author: @ruanlcs
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        
+        int y = Convert.ToInt32(Console.ReadLine());
+        
+        int tempo = 2*y;
+        
+        Console.WriteLine(tempo + " minutos");
+
+    }
+
+}
