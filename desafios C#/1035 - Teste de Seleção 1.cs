1a12a7f35b5bc87e5a40b53f6b9e0edae5d00d52
Ruan Lucas <142461245+ruanlcs@users.noreply.github.com>
2024-02-23 15:33:32 -0300

    Create 1035 - Teste de Seleção 1.cs

---
+++ desafios C#/1035 - Teste de Seleção 1.cs
@@ -0,0 +1,24 @@
+//Author: @ruanlcs
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+    String line = Console.ReadLine();
+    string[] valor = line.Split(' ');
+    int A = Convert.ToInt32(valor[0]);
+    int B = Convert.ToInt32(valor[1]);
+    int C = Convert.ToInt32(valor[2]);
+    int D = Convert.ToInt32(valor[3]);
+    
+
+    if(B > C && D > A && (C+D) > (A+B) && C>0 && D>0 && (A%2==0)){
+        Console.WriteLine("Valores aceitos");
+    } else {
+        Console.WriteLine("Valores nao aceitos");
+    }
+
+    }
+
+}
