93e89fd33c05c8abb3cc18fa40fa8eca305151cc
Ruan Lucas <142461245+ruanlcs@users.noreply.github.com>
2024-04-12 22:00:46 -0300

    Create 1049 - Animal.cs

---
+++ desafios C#/1049 - Animal.cs
@@ -0,0 +1,28 @@
+//author:@ruanlcs
+class URI {
+
+    static void Main(string[] args) { 
+
+    string c1 = Console.ReadLine();
+    string c2 = Console.ReadLine();
+    string c3 = Console.ReadLine();
+     if(c1 == "vertebrado" && c2== "ave" && c3 == "carnivoro")
+     {Console.WriteLine("aguia");}
+     if(c1 == "vertebrado" && c2=="ave" && c3=="onivoro")
+     {Console.WriteLine("pomba");}
+     if(c1=="vertebrado" && c2=="mamifero" && c3=="onivoro")
+     {Console.WriteLine("homem");}
+     if(c1=="vertebrado" && c2=="mamifero" && c3=="herbivoro")
+     {Console.WriteLine("vaca");}
+     if(c1=="invertebrado" && c2=="inseto" && c3=="hematofago")
+     {Console.WriteLine("pulga");}
+     if(c1=="invertebrado" && c2=="inseto" && c3=="herbivoro")
+     {Console.WriteLine("lagarta");}
+     if(c1=="invertebrado" && c2=="anelideo" && c3=="hematofago")
+     {Console.WriteLine("sanguessuga");}
+     if(c1=="invertebrado" && c2=="anelideo" && c3=="onivoro")
+     {Console.WriteLine("minhoca");}
+
+    }
+
+}
